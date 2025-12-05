28a47672a9f3b06c1c369dad0cbeb05628780195
smnsdra <sammansedra2@gmail.com>
2025-12-05 22:12:45 +0200

    Replace BrowserRouter with HashRouter in index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './styles.css'; // project overrides
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Derive a safe basename from PUBLIC_URL so the router works when "homepage" is set to a full URL
-// CRA sets process.env.PUBLIC_URL from the "homepage" field in package.json.
-// If PUBLIC_URL is a full URL (eg. https://user.github.io/repo), new URL(...).pathname returns "/repo".
-const basename = process.env.PUBLIC_URL
-  ? (() => {
-      try {
-        return new URL(process.env.PUBLIC_URL).pathname;
-      } catch {
-        return process.env.PUBLIC_URL;
-      }
-    })()
-  : '/';
-
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
