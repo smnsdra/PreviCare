db57d1fd3dbed848eb795ba5ae2127ac06048a07
smnsdra <sammansedra2@gmail.com>
2025-12-31 19:26:22 +0200

    Refactor DB connection to use new env variables
    Updated database connection settings to use new environment variables.

--- backend/db.js
+++ backend/db.js
@@ -2,13 +2,15 @@ const mysql = require('mysql2');
 
 const pool = mysql
   .createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'previcare',
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
     waitForConnections: true,
-    connectionLimit: parseInt(process.env.DB_CONN_LIMIT || '10', 10),
+    connectionLimit: 10,
     queueLimit: 0,
+    ssl: { rejectUnauthorized: false } // 🔴 مهم مع Railway
   })
   .promise();
 
