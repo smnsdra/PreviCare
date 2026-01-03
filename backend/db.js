b62d15558ceb2191fd60df7887046d21c0502e4c
smnsdra <sammansedra2@gmail.com>
2026-01-03 05:31:32 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -4,9 +4,11 @@ if (!process.env.MYSQL_PUBLIC_URL) {
   throw new Error("❌ MYSQL_PUBLIC_URL is missing");
 }
 
-const pool = mysql
-  .createPool(process.env.MYSQL_PUBLIC_URL)
-  .promise();
+const pool = mysql.createPool({
+  uri: process.env.MYSQL_PUBLIC_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-module.exports = pool;
-
+module.exports = pool.promise();
